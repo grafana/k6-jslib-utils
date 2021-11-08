1ff6e90521a3b689facff72a8c2c0648647e050d
kwidera <krzysztof.widera@gmail.com>
2021-11-08 12:36:32 +0100

    name changes and good practices implemented

--- src/utils.js
+++ src/utils.js
@@ -33,31 +33,32 @@ export function findBetween(content, left, right) {
   return content.substring(start, end);
 }
 
-function normalDensity(mean, scale, x) {
-  return Math.exp(-1/2 * Math.pow((x - mean)/scale, 2)) / (scale * Math.sqrt(2 * Math.PI))
-}
+export function normalDistributionStages(maxVus, durationSeconds, numberOfStages=10) {
+  function normalDensity(mean, scale, x) {
+    return Math.exp(-1 / 2 * Math.pow((x - mean) / scale, 2)) / (scale * Math.sqrt(2 * Math.PI));
+  }
 
-export function normalDistributionStages(maxVU, duration, noStages = 1) {
   const mean = 0;
   const scale = 1;
-  //changing above will change the curve
-  let value = new Array(noStages+2).fill(0);
-  let durations = new Array(noStages+2).fill(Math.ceil(duration/6));
-  let normalStages = [];
+  let curve = new Array(numberOfStages + 2).fill(0);
+  let durations = new Array(numberOfStages + 2).fill(Math.ceil(durationSeconds / 6));
+  let k6stages = [];
 
-  for(let i = 0; i <= noStages; i++) {
-      value[i] = normalDensity(mean, scale, -2*scale + 4*scale*i/noStages)
-  };
-  let maxValue = Math.max(...value);
-  let users = value.map(x => Math.round(x * maxVU / maxValue));
+  for (let i = 0; i <= numberOfStages; i++) {
+    curve[i] = normalDensity(mean, scale, -2 * scale + 4 * scale * i / numberOfStages);
+  }
 
-  for (let j = 1; j <= noStages; j++) {
-      durations[j] = Math.ceil(4*duration/(6*noStages))
-  };
+  let peakDistribution = Math.max(...curve);
 
-  for (let k = 0; k <= noStages+1; k++) {
-      normalStages.push({duration: `${durations[k]}s`, target: users[k]})
-  };
+  let vus = curve.map(x => Math.round(x * maxVus / peakDistribution));
 
-  return normalStages
+  for (let j = 1; j <= numberOfStages; j++) {
+    durations[j] = Math.ceil(4 * durationSeconds / (6 * numberOfStages));
+  }
+
+  for (let k = 0; k <= numberOfStages + 1; k++) {
+    k6stages.push({duration: `${durations[k]}s`, target: vus[k]});
+  }
+
+  return k6stages;
 }
