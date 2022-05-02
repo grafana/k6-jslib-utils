b76cd8f069f1b29fb87e9be987aa9146036eb081
Ivan <2103732+codebien@users.noreply.github.com>
2022-05-02 15:35:31 +0200

    Use index.js as an entrypoint for webpack

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import {
+	parseDuration,
+	getCurrentStageIndex,
+	tagWithCurrentStageIndex,
+	tagWithCurrentStageProfile,
+} from './stages.js'
+
+import {
+	findBetween,
+	normalDistributionStages,
+	randomIntBetween,
+	randomItem,
+	randomString,
+	uuidv4,
+} from './utils.js'
+
+export {
+	parseDuration,
+	getCurrentStageIndex,
+	tagWithCurrentStageIndex,
+	tagWithCurrentStageProfile,
+	findBetween,
+	normalDistributionStages,
+	randomIntBetween,
+	randomItem,
+	randomString,
+	uuidv4,
+}
