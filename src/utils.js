89228033aea3bce7c33308a99969ff157318c124
oleiade <theo@crevon.me>
2022-05-03 11:05:33 +0200

    Add a secure flag to uuidv4 to support cryptographically secure uuids
    Our initial implementation of UUIDv4 used Math.Random(). This commit
    adds a secure flag to the function which, when set to true, sets the
    function to produce a cryptographically secure UUID instead.

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,13 @@
-export function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    let r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
+import { randomBytes } from "k6/crypto";
+
+/**
+ * Produces a Universal Unique Identifier version 4
+ *
+ * @param {boolean} secure - whether a cryptographically secure generation function should be used
+ * @returns {String} - 16 characters hexadecimal representation of the UUID v4
+ */
+export function uuidv4(secure = false) {
+  return secure ? secureUUIDv4() : insecureUUIDv4();
 }
 
 export function randomIntBetween(min, max) { // min and max included
@@ -80,4 +85,75 @@ export function normalDistributionStages(maxVus, durationSeconds, numberOfStages
   }
 
   return k6stages;
+}
+
+/**
+ * Fast UUID v4 producer
+ *
+ * Note that this function does not produce cryptographically
+ * secure UUIDs. If you need safe UUIDs, use the secureUUIDv4
+ * implementation instead.
+ *
+ * @returns {String} - 16 characters hexadecimal representation of the UUID v4
+ */
+function insecureUUIDv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    let r = (Math.random() * 16) | 0,
+      v = c === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
+/**
+ * Cryptographically secure UUID v4 producer
+ *
+ * Note that this implementation involves a computation
+ * overhead.
+ *
+ * @returns {String} - 16 characters hexadecimal representation of the UUID v4
+ */
+function secureUUIDv4() {
+  /**
+   * bytes conversion table used to convert UUIDs bytes
+   * to string form.
+   */
+  const byteToHex = [];
+  for (let i = 0; i < 256; ++i) {
+    byteToHex.push((i + 0x100).toString(16).slice(1));
+  }
+
+  /**
+   * produce 16 random bytes, and set UUID v4's specific
+   * version and `clock_seq_hi_and_reserved` bits.
+   */
+  const rnds = new Uint8Array(randomBytes(16));
+  rnds[6] = (rnds[6] & 0x0f) | 0x40;
+  rnds[8] = (rnds[8] & 0x3f) | 0x80;
+
+  /**
+   * Convert array of 16 byte values to UUID string format of the form:
+   * XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
+   */
+  return (
+    byteToHex[rnds[0]] +
+    byteToHex[rnds[1]] +
+    byteToHex[rnds[2]] +
+    byteToHex[rnds[3]] +
+    "-" +
+    byteToHex[rnds[4]] +
+    byteToHex[rnds[5]] +
+    "-" +
+    byteToHex[rnds[6]] +
+    byteToHex[rnds[7]] +
+    "-" +
+    byteToHex[rnds[8]] +
+    byteToHex[rnds[9]] +
+    "-" +
+    byteToHex[rnds[10]] +
+    byteToHex[rnds[11]] +
+    byteToHex[rnds[12]] +
+    byteToHex[rnds[13]] +
+    byteToHex[rnds[14]] +
+    byteToHex[rnds[15]]
+  ).toLowerCase();
 }
