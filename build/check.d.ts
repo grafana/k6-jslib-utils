fa74d495868666d44d94573a1f5ecf61b588e387
Johan Suleiko Allansson <allansson@gmail.com>
2024-08-13 11:30:53 +0200

    Revert "remove built type definitions"
    This reverts commit 25b540dbcc5c7eaec47d85e8f94d7945adec1066.

---
+++ build/check.d.ts
@@ -0,0 +1,12 @@
+type FalsyCheckValue = Array<unknown> | Record<string | number | symbol, unknown> | boolean | string | number | null | undefined;
+type Checker<T> = ((value: T) => FalsyCheckValue) | ((value: T) => Promise<FalsyCheckValue>) | FalsyCheckValue | Promise<FalsyCheckValue>;
+interface CheckMap<T> {
+    [key: string]: Checker<T>;
+}
+interface SyncCheckMap {
+    [key: string]: ((value: any) => FalsyCheckValue) | FalsyCheckValue;
+}
+type CheckResult<C extends CheckMap<any>> = C extends SyncCheckMap ? boolean : Promise<boolean>;
+export declare function check<C extends CheckMap<any>>(checkers: C): CheckResult<C>;
+export declare function check<T, C extends CheckMap<T>>(value: T, checkers: C): CheckResult<C>;
+export {};
