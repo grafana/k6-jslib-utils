f01e1f05ca5beda8647f960f38e544848a0ee77a
Johan Suleiko Allansson <allansson@gmail.com>
2024-08-16 11:12:36 +0200

    build files

--- build/check.d.ts
+++ build/check.d.ts
@@ -1,10 +1,10 @@
-type FalsyCheckValue = Array<unknown> | Record<string | number | symbol, unknown> | boolean | string | number | null | undefined;
-type Checker<T> = ((value: T) => FalsyCheckValue) | ((value: T) => Promise<FalsyCheckValue>) | FalsyCheckValue | Promise<FalsyCheckValue>;
+type CheckValue = Array<unknown> | Record<string | number | symbol, unknown> | boolean | string | number | null | undefined;
+type Checker<T> = ((value: T) => CheckValue) | ((value: T) => Promise<CheckValue>) | CheckValue | Promise<CheckValue>;
 interface CheckMap<T> {
     [key: string]: Checker<T>;
 }
 interface SyncCheckMap {
-    [key: string]: ((value: any) => FalsyCheckValue) | FalsyCheckValue;
+    [key: string]: ((value: any) => CheckValue) | CheckValue;
 }
 type CheckResult<C extends CheckMap<any>> = C extends SyncCheckMap ? boolean : Promise<boolean>;
 export declare function check<T, C extends CheckMap<T>>(value: T, checkers: C, tags?: Record<string, string>): CheckResult<C>;
